ff38300a0fac9d0294dde94dc8de16d3272a9eae
Sachuriga <sachuriga3@gmail.com>
2024-05-13 16:58:56 +0200

    0.6.3.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_requirements():
             return [line.strip() for line in req if line.strip() and not line.startswith('#')]
 setup(
     name='nwb4fp',
-    version='0.6.2.8',
+    version='0.6.3.1',
     url='https://github.com/sachuriga/QuattrocoloLab-nwb4fp',
     author='sachuriga',
     author_email='sachuriga.sachuriga@ntnu.no',
